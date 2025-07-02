7e10d09763f9f572a4393dbf141277d652eaf68a
Bladers123 <tristangehring123@hotmail.de>
2025-07-02 23:00:15 +0200

    create project modal component

---
+++ src/app/pages/project-modal/project-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-modal',
+  imports: [],
+  templateUrl: './project-modal.html',
+  styleUrl: './project-modal.scss'
+})
+export class ProjectModalComponent {
+
+}
